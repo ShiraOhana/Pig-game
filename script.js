2c5af0141b5789311011ab1b46f9c71c72fde5ce
ShiraOhana <soshiraohana@gmail.com>
2023-01-06 19:56:02 +0200

    dice functionality

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
 
 // Rolling the dice functionality
 btnRoll.addEventListener('click', function () {
@@ -22,5 +26,12 @@ btnRoll.addEventListener('click', function () {
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
 
-  //3. Check for a rolled 1 : if true -> switch to next player
+  //3. Check for a rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //CHANGE LATER
+  } else {
+    // switch to next player
+  }
 });
