42617e5845d349385016031d2d0e062d96b0ab0d
ShiraOhana <soshiraohana@gmail.com>
2023-01-07 22:28:23 +0200

    new game btn and init function

--- script.js
+++ script.js
@@ -12,14 +12,26 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player1El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -52,6 +64,8 @@ btnRoll.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
 
 btnHold.addEventListener('click', function () {
   if (playing) {
